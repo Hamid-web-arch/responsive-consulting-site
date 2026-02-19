533e42d73b5347cecc6c7d0040bc8f7e015c6f10
Həmid Ağabəyov <148481988+Hamid-web-arch@users.noreply.github.com>
2026-02-20 01:03:23 +0400

    hamburger menu click funcktion added

---
+++ script.js
@@ -0,0 +1,8 @@
+    
+    /* HAMBURGER MENU OPEN CLOSE */
+   let hamburgermenu = document.querySelector(`.hamburger-menu-list`);
+   function OpneClose() {
+       hamburgermenu.classList.toggle(`transform`);
+   }
+
+   
